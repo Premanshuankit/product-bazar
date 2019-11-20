6dab41d1807f899a13274a86bcf4e30cae55370e
Premanshu Ankit <ankit.premanshu@gmail.com>
2019-11-20 19:15:51 +0530

    cards added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PBMaterialModule } from './material-module';
+import { HomeComponent } from './components/home/home.component';
+// import { ProductsModule } from './modules/products/products.module';
+// for lazy loading ProductsModule is commented
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    PBMaterialModule,
+    // ProductsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/products/products/products.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductDataService } from '../product-data.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent implements OnInit {
+
+  products: Observable<any>;
+  constructor(private productDataService: ProductDataService) { }
+
+  ngOnInit() {
+    this.products = this.productDataService.getAllProducts();
+  }
+
+}
